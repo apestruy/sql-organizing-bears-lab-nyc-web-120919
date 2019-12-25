2fc878a92c291c0c50559d8fed26853afc59a12f
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-25 04:22:04 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO bears (name, age) VALUES ("Mr. Chocolate", 3);
-INSERT INTO bears (name, age) VALUES ("Mr. Chocolate", 3);
+INSERT INTO bears (name, age) VALUES ("Rowdy", 6);
 INSERT INTO bears (name, age) VALUES ("Mr. Chocolate", 3);
 INSERT INTO bears (name, age) VALUES ("Mr. Chocolate", 3);
 INSERT INTO bears (name, age) VALUES ("Mr. Chocolate", 3);
